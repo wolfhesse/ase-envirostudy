dfde9a07685ab008b8c520cf0f10913f115fdac1
wolfhesse.core <wolfgang.schuessel@gmail.com>
2016-01-18 22:13:33 +0100

    cursor system

--- eins.rb
+++ eins.rb
@@ -1,5 +1,13 @@
 require "prawn"
 
 Prawn::Document.generate("hello.pdf") do
-  text "Hello World!"
+	stroke_axis
+	text "the cursor is here: #{cursor}"
+	text "now it is here: #{cursor}"
+	move_down 200
+	text "on the first move the cursor went down to: #{cursor}"
+	move_up 100
+	text "on the second move the cursor went up to: #{cursor}"
+	move_cursor_to 50
+	text "on the last move the cursor went directly to: #{cursor}"
 end
